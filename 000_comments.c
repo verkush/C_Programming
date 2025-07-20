b3f9cd94f32823dc4ab72a041f342fb40182d16f
Ayush Kumar Verma <ayushkumarverma97@outlook.com>
2025-07-21 01:31:45 +0530

    [Basics] Comments in C Programming Language

---
+++ 000_comments.c
@@ -0,0 +1,14 @@
+/*
+In C programming, comments are used to explain code, 
+making it easier to understand and maintain. 
+Comments are ignored by the compiler. 
+There are two types of comments in C:
+*/
+
+// Single line comment
+
+/*
+    This is a 
+    Multi-Line
+    Comment
+*/
